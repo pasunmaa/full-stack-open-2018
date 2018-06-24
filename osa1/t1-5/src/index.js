50daff957ff3c2328014682c272899198b6ed601
Petri Asunmaa <petrieas@gmail.com>
2018-06-24 11:32:52 +0300

    Tehtävä 1.2 valmis

--- osa1/t1-5/src/index.js
+++ osa1/t1-5/src/index.js
@@ -9,12 +9,19 @@ const Otsikko = (props) => {
       )
 }
 
+const Osa = (props) => {
+    return (
+        <div>
+            <p>{props.osa}: {props.tehtavia} tehtävää</p>
+        </div>
+      )
+}
 const Sisalto = (props) => {
     return (
         <div>
-            <p>{props.osa1}: {props.tehtavia1} tehtävää</p>
-            <p>{props.osa2}: {props.tehtavia2} tehtävää</p>
-            <p>{props.osa3}: {props.tehtavia3} tehtävää</p>
+            <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+            <Osa osa={props.osa2} tehtavia={props.tehtavia3} />
+            <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
         </div>
       )
 }
