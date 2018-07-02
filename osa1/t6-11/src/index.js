7dab9c0791239c713994fc13aafbf2657af08f0a
Petri Asunmaa <petrieas@gmail.com>
2018-07-02 10:06:04 +0300

    Tehtävä 1.7 valmis

--- osa1/t6-11/src/index.js
+++ osa1/t6-11/src/index.js
@@ -13,10 +13,19 @@ class App extends React.Component {
     }
   
     render() {
+        const summa = (a, b, c) => a + b + c
+        const keskiarvo = (a, b, c) => (a * 1 + b * 0 + c * -1) / summa(a, b, c)
+        const naytakeskiarvo = (a, b, c) => summa(a, b, c) ? keskiarvo(a, b, c) : 0 
+        const positiivisia = (a, b, c) => a / summa(a, b, c)
+        const naytapositiivisia = (a, b, c) => summa(a, b, c) ? roundToOne(positiivisia(a, b, c) * 100) : 0
+        const roundToOne = (num) => {    
+            return + (Math.round(num + "e+1")  + "e-1");
+        }
+        
         const setToValue = (valueitem, newValue) => () => { 
-            if (valueitem == "hyva")
+            if (valueitem === "hyva")
                 this.setState({ hyva: newValue })
-            else if (valueitem == "neutraali")
+            else if (valueitem === "neutraali")
                 this.setState({ neutraali: newValue })
             else 
                 this.setState({ huono: newValue })
@@ -33,6 +42,8 @@ class App extends React.Component {
             <ul>hyvä: {this.state.hyva}</ul>
             <ul>neutraali: {this.state.neutraali}</ul>
             <ul>huono: {this.state.huono}</ul>
+            <ul>keskiarvo: {naytakeskiarvo(this.state.hyva, this.state.neutraali, this.state.huono)}</ul>
+            <ul>positiivisia: {naytapositiivisia(this.state.hyva, this.state.neutraali, this.state.huono)} %</ul>
         </div>
         )
     }
