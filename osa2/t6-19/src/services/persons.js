e6670979efc5b8ec958d310cda6703d2cd4971a7
Petri Asunmaa <petrieas@gmail.com>
2018-07-15 12:29:56 +0300

    Tehtävä 3.10 : front vie nyt pilvipalveluun

--- osa2/t6-19/src/services/persons.js
+++ osa2/t6-19/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://petria-puhelinluettelo-3-10.now.sh/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
